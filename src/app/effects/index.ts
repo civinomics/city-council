2b96fa98126986e49a332a5d1034067736869af2
Drew Moore <drewmoore@ucsc.edu>
2017-03-22 18:19:03 -0400

    remove inadvertently added folder

--- src/app/effects/index.ts
+++
@@ -1,7 +0,0 @@
-import { FocusStateEffects } from './focus-state.effects';
-import { EffectsModule } from '@ngrx/effects';
-export const EFFECTS = [
-  FocusStateEffects
-];
-
-export const RUN_EFFECTS = EFFECTS.map(it => EffectsModule.run(it));
