86d121e5f8cb721917f0d602b8de75cf16e5b71e
Drew Moore <drewmoore@ucsc.edu>
2017-04-17 17:32:35 -0400

    merge package-by-feature branch into master

--- cloud/functions/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var create_user_records_1 = require("./src/create-user-records");
-exports.createUserRecords = create_user_records_1.createUserRecords;
-var check_districts_1 = require("./src/check-districts");
-exports.districts = check_districts_1.districts;
-var meeting_stats_1 = require("./src/meeting-stats");
-exports.stats = meeting_stats_1.stats;
-//# sourceMappingURL=index.js.map
